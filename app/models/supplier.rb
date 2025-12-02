972c76983a666d0a4d4f80dcfa2624cfa6168422
Monkee45 <michael.tom.walsh@gmail.com>
2025-12-02 12:05:29 +1100

    added Rubocop

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,16 +1,11 @@
 class Supplier < ApplicationRecord
   has_many :reviews, dependent: :destroy
-
-validates :abn, format: { with: /\A\d{11}\z/, message: "Incorrect Format - should be 11 numerical digits" }
-
-validates :registered_name, :abn, presence: true
-
-SCREEN_STATUS = %w[SCREENED NOT-SCREENED]
-
-validates :screening_status, inclusion: { in: SCREEN_STATUS }
+  validates :abn, format: { with: /\A\d{11}\z/, message: "Incorrect Format - should be 11 numerical digits" }
+  validates :registered_name, :abn, presence: true
+  SCREEN_STATUS = %w[SCREENED NOT-SCREENED]
+  validates :screening_status, inclusion: { in: SCREEN_STATUS }
 
   def self.screened
     where("screening_status == 'SCREENED'")
   end
-
 end
